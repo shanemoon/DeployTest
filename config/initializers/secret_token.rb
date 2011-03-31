e09ba8d9d9dd06e4556a19b991d531d41e7ffd4d
unknown <smoon@.olin.edu>
2011-03-31 11:59:28 -0400

    asdf

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeployTest::Application.config.secret_token = '1dad508e1b419ba87db6f938ecbe04d6e0fd0cce0a1d5712b743e073c80006a766e3f627434d22da5bd527abeb520daa41228689507d9766595ae0423a77f090'
